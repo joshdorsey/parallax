f61fae989e7ae57dd7ceab12c67d2e6e5e8c60ce
Josh Dorsey <happyhillsbutchery@gmail.com>
2015-04-12 21:38:56 -0700

    - Fixed different resolutions

--- js/parallax.js
+++ js/parallax.js
@@ -10,12 +10,12 @@ var viewPlaneDist = 1;
 var objects = [];
 
 function updateObjects(e){
-	var posX = screen.width-e.clientX-(screen.width/2);
-	var posY = e.clientY-(screen.height/2);
+	var posX = window.innerWidth-e.clientX-(window.innerWidth/2);
+	var posY = e.clientY-(window.innerHeight/2);
 	for(var i = 0; i < objects.length; i++){
 		objects[i].pos = posX+objects[i].off;
-		$(objects[i].el).css("left", (screen.width/2)+screenDist(objects[i].d, posX, viewPlaneDist)-($(objects[i].el).width()/2)+"px");
-		$(objects[i].el).css("bottom", (screen.height/2)+screenDist(objects[i].d, posY, viewPlaneDist)-($(objects[i].el).height()/2)+"px");
+		$(objects[i].el).css("left", (window.innerWidth/2)+screenDist(objects[i].d, posX, viewPlaneDist)-($(objects[i].el).width()/2)+"px");
+		$(objects[i].el).css("bottom", (window.innerHeight/2)+screenDist(objects[i].d, posY, viewPlaneDist)-($(objects[i].el).height()/2)+"px");
 	}
 }
 
