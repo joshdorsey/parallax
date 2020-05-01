4dd656ccba6184f67ef7eda83d9a789cf18277af
Josh Dorsey <dorseyja98@gmail.com>
2020-05-01 02:16:03 -0600

    Removed info text

--- js/parallax.js
+++ js/parallax.js
@@ -6,16 +6,16 @@ function object(element, position, dist){
 	return {el: element, off: position, pos: 0, d: dist};
 }
 
-var viewPlaneDist = 1;
+var viewPlaneDist = 1.;
 var objects = [];
 
 function updateObjects(e){
-	var posX = window.innerWidth-e.clientX-(window.innerWidth/2);
-	var posY = e.clientY-(window.innerHeight/2);
+	var posX = (window.innerWidth/2.)-e.clientX;
+	var posY = e.clientY-(window.innerHeight/2.);
 	for(var i = 0; i < objects.length; i++){
 		objects[i].pos = posX+objects[i].off;
-		$(objects[i].el).css("left", (window.innerWidth/2)+screenDist(objects[i].d, posX, viewPlaneDist)-($(objects[i].el).width()/2)+"px");
-		$(objects[i].el).css("bottom", (window.innerHeight/2)+screenDist(objects[i].d, posY, viewPlaneDist)-($(objects[i].el).height()/2)+"px");
+		$(objects[i].el).css("left", (window.innerWidth/2.)+screenDist(objects[i].d, posX, viewPlaneDist)-($(objects[i].el).width()/2.)+"px");
+		$(objects[i].el).css("bottom", (window.innerHeight/2.)+screenDist(objects[i].d, posY, viewPlaneDist)-($(objects[i].el).height()/2.)+"px");
 	}
 }
 
